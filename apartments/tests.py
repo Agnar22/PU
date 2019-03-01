23523efa34df61c13d63f2bf34417d3d4c302862
Tobias <tobias.wulvik@gmail.com>
2019-03-01 11:45:15 +0100

    Creating tests

--- apartments/tests.py
+++ apartments/tests.py
@@ -1,37 +1,33 @@
-from django.test import TestCase
+import datetime
+
+from django.test import TestCase, RequestFactory
 
 # Create your tests here.
+from django.urls import reverse
+from faker import Faker
+from mixer.backend.django import mixer
+
 from apartments.models import Apartment, Contract
+from apartments.views import apartment_detail, apartments
 from authentication.models import Profile
 
 
 class ApartmentTest(TestCase):
+    def test_apartment_list(self):
+        path = '/apartments/?location=Trondheim&start_date=2019-03-01&end_date=2019-04-01&guests=1'
+        request = RequestFactory().get(path)
+        request.content_params
+        response = apartments(request)
 
-    # def create_contract(self):
-    #     return Contract.objects.create(contract_text="dsadsa",
-    #                                    tenant=self.create_user(),
-    #                                    start_date="2019-02-01",
-    #                                    end_date="2019-03-04")
-    #
-    # def create_user(self):
-    #     return Profile.objects.create(email="tobias.wulvik@gmail.com",
-    #                                   first_name="Tobias",
-    #                                   last_name="Wulvik",
-    #                                   phone_number=47244448)
-    #
-    # def create_apartment(self):
-    #     return Apartment.objects.create(title="Testleilighet",
-    #                                     description="abcdefg",
-    #                                     country="Norge",
-    #                                     city="Trondheim",
-    #                                     address="Aslak bolts gate 3",
-    #                                     beds=4,
-    #                                     apartment_age=1966,
-    #                                     rating=4,
-    #                                     monthly_cost=5000,
-    #                                     size=43,
-    #                                     owner=self.create_user(),
-    #                                     contracts=self.create_contract())
+        self.assertEqual(response.status_code, 200)
 
-    def test_apartment_creation(self):
-        self.assertEqual(True, True)
+    def test_apartment_detail(self):
+        apartment = mixer.blend(Apartment)
+        pk = apartment.pk
+        start_date = '2019-03-19'
+        end_date = '2019-04-19'
+        path = reverse('apartment-detail', kwargs={'apartment-id': pk, 'start-date': start_date, 'end-date': end_date})
+        request = RequestFactory().get(path)
+        response = apartment_detail(request, pk, start_date, end_date)
+
+        self.assertEqual(response.status_code, 200)
