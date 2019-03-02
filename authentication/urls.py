b427c2fe05775b87b46baf25c7bd89ac8610ab2f
Tobias <tobias.wulvik@gmail.com>
2019-03-02 11:41:45 +0100

    Merge request errors after project restructure. Now fixed

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,12 +6,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.LoginFormView, name='login'),
-]
-
-urlpatterns = [
     path('logout/', views.logout_user, name='logout'),
-]
-
-urlpatterns = [
     path('register/', views.RegisterFormView, name='register'),
 ]
