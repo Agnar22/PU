107fc56eaf8ada5168025603a9495286ab9ad05d
Tobias <tobias.wulvik@gmail.com>
2019-03-06 18:28:21 +0100

    Added snackbars on landingpage

--- landingpage/views.py
+++ landingpage/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 
 from authentication.forms import RegisterForm, LoginForm
 from authentication.models import Profile
+from django.contrib import messages
 
 
 def landing_page(request):
@@ -32,17 +33,20 @@ def landing_page(request):
                 if user is not None:
                     if user.is_active:
                         login(request, user)  # User is logged in
+                        messages.success(request, 'Du er nå registrert og logget inn!')
                         return redirect('/')
-
+            else:
+                messages.warning(request, form.errors)
             return render(request, 'landingpage/landing-page.html', {'form': form})
         else:
-            print("Login")
             email = request.POST.get("email")
             password = request.POST.get("password")
             user = authenticate(request, email=email, password=password)
             if user is not None:
                 login(request, user)
+                messages.success(request, 'Du er nå logget inn!')
                 return redirect('landing-page')
             else:
+                messages.error(request, 'Fant dessverre ikke brukeren, prøv på nytt')
                 return redirect('landing-page')
 
