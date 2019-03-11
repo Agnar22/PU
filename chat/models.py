887570921c1817d2c0b3d0a1097dd657928196dc
Joel Kristian Nicolaysen <>
2019-03-11 12:29:00 +0100

    Model til Message

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+import datetime
+
+from authentication.models import Profile
+
 # Create your models here.
+class Message(models.Model):
+    content = models.TextField()
+    messager = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="messager", null=True)
+    reciever = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="reciever", null=True)
+    time = models.DateField()
