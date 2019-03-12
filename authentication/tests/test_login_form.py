656ee601ff665cd7f770cff0ecc46e47f305f70f
Tor Martin Wang <tmwang@stud.ntnu.no>
2019-03-12 19:13:44 +0100

    Restored almost all tests, will try to merge with master to see if tests run successfully then

--- authentication/tests/test_login_form.py
+++ authentication/tests/test_login_form.py
@@ -1,22 +1,22 @@
-# from django.test import TestCase, RequestFactory
-# from apartments.models import Apartment
-# from authentication.models import Profile
-# from authentication.forms import LoginForm
-#
-#
-# class RegisterFormTest(TestCase):
-#     def test_form_no_data(self):
-#         form = LoginForm(data={})
-#         self.assertFalse(form.is_valid())
-#         self.assertEqual(len(form.errors), 2)
-#
-#     def test_form_valid_data(self):
-#         form = LoginForm(data={
-#             'email': "test@test.test",
-#             'password': "TestPassword"
-#         })
-#
-#         self.assertTrue(form.is_valid())
-#         self.assertEqual(len(form.errors), 0)
-#
-# # python manage.py test authentication
+from django.test import TestCase, RequestFactory
+from apartments.models import Apartment
+from authentication.models import Profile
+from authentication.forms import LoginForm
+
+
+class RegisterFormTest(TestCase):
+    def test_form_no_data(self):
+        form = LoginForm(data={})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 2)
+
+    def test_form_valid_data(self):
+        form = LoginForm(data={
+            'email': "test@test.test",
+            'password': "TestPassword"
+        })
+
+        self.assertTrue(form.is_valid())
+        self.assertEqual(len(form.errors), 0)
+
+# python manage.py test authentication
