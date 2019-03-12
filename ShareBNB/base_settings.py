efb8826460663002cc986c63ace3506ce0ecffb1
Tor Martin Wang <tmwang@stud.ntnu.no>
2019-03-12 15:07:55 +0100

    Third attempt at making CI tests work

--- ShareBNB/base_settings.py
+++ ShareBNB/base_settings.py
@@ -14,7 +14,7 @@ import os
 import django_heroku
 
 #This constant needs to be here in order for the tests to run successfully
-STATICFILES_STORAGE = 'my_project.storage.WhiteNoiseStaticFilesStorage'
+STATICFILES_STORAGE = 'gruppe-15.storage.WhiteNoiseStaticFilesStorage'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
