e6b2ce018935e5706cff615bbd991c8cdc0a99b0
Tobias <tobias.wulvik@gmail.com>
2019-03-20 16:47:58 +0100

    HTML emails

--- ShareBNB/urls.py
+++ ShareBNB/urls.py
@@ -26,7 +26,8 @@ urlpatterns = [
     path('', include('landingpage.urls')),
     path('apartments/', include('apartments.urls')),
     path('', include('profile_page.urls')),
-    path('review/', TemplateView.as_view(template_name='review_user_apartment.html'))
+    path('review/', TemplateView.as_view(template_name='review_user_apartment.html')),
+    path('review-email/', TemplateView.as_view(template_name='email/review-email.html'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
