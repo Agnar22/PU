f4cb4bbd7e7cf71da6754222350973830a57323c
Tor Martin Wang <tmwang@stud.ntnu.no>
2019-03-21 09:07:12 +0100

    Fixed so that uploaded images are stored in the same folder as 'gruppe-15'

--- ShareBNB/dev_settings.py
+++ ShareBNB/dev_settings.py
@@ -2,5 +2,5 @@ from ShareBNB.base_settings import *
 
 # Override base.py settings here
 
-DEBUG = False
-MEDIA_ROOT = os.path.join(BASE_DIR, '..', '..', 'assets', 'media')
+DEBUG = True
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
