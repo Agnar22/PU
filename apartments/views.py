0c069d95b853ae35c011512d363f08477cb2dec8
Tor Martin Wang <tmwang@stud.ntnu.no>
2019-03-30 12:56:23 +0100

    Users can now press a button in the apartment-detail to send a message to the owner of the apartment. The message button and the 'make contract' button are now hid den if the apartment you're viewing is your own. Also fixed site-breaking bug that happens when there are contracts that are not tied to any apartments (which would happen if an apartment got deleted)

--- apartments/views.py
+++ apartments/views.py
@@ -138,7 +138,6 @@ def apartment_detail(request, apartment_id, start_date, end_date):
 
     apartment = Apartment.objects.get(pk=apartment_id)
 
-    print("HEHUE")
     apartment_price = apartment.calculate_price(start_date, end_date)
     start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d")
     end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d")
